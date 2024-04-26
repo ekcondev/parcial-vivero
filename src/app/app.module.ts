c6aacbc7b2dad0ff8767a6e92ce477a5618b813f
ekcondev <e.condev@uniandes.edu.co>
2024-04-25 21:36:48 -0500

    Creacion servicio para obtener las plantas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration  } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantaModule } from './planta/planta.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { PlantaModule } from './planta/planta.module';
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    PlantaModule
+    PlantaModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
