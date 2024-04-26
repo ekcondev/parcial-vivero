ab47dad420751079ed029d717afe0027e510bc62
ekcondev <e.condev@uniandes.edu.co>
2024-04-26 00:51:51 -0500

    Prueba del componente

--- src/app/planta/planta-list/planta-list.component.spec.ts
+++ src/app/planta/planta-list/planta-list.component.spec.ts
@@ -2,27 +2,66 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantaListComponent } from './planta-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantaListComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [PlantaListComponent],
+      providers: [PlantaService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const planta = new Planta(
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+      component.plantas.push(planta);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Comprobar que se crea la tabla
+  it('Component has a table', () => {
+    expect(debug.query(By.css('tbody')).childNodes.length).toBeGreaterThan(0);
+  });
+
+  // Comprobar que la tabla tiene el encabezado
+  it('Component table has a thead', () => {
+    const tablaElement: HTMLElement = fixture.nativeElement;
+    const encabezado = tablaElement.querySelector('thead');
+    expect(encabezado).toBeTruthy();
+  });
+
+  // Comprobar que la tabla tiene 3 filas
+  it('Component table has 4 tr elements (head and rows with data)', () => {
+    const tablaElement: HTMLElement = fixture.nativeElement;
+    const filas = tablaElement.querySelectorAll('tr');
+    expect(filas.length).toBe(4);
   });
 });
